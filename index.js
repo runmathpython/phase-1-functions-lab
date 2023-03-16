d07e76d39e1f13ec67091bc7618f1df0500c7f52
Seong Kim <runmath@gmail.com>
2023-03-15 23:39:45 -0600

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    if (street > 42) {
+        return street - 42;
+    } else {
+        return 42 - street;
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return (destination - start) * 264;
+    } else {
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceCovered = distanceTravelledInFeet(start, destination);
+    if (distanceCovered <= 400) {
+        return 0;
+    } else if ((distanceCovered > 400) && (distanceCovered <= 2000)) {
+        return (distanceCovered - 400) * 0.02;
+    } else if ((distanceCovered > 2000) && (distanceCovered <= 2500)) {
+        return 25;
+    } else {
+        return "cannot travel that far"
+    }
+}
